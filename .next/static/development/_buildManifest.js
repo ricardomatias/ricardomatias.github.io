8cc8826c87eb8981cc0215df53ed9edac7054ec5
Ricardo Matias <ricardo.matias0@gmail.com>
2020-08-31 19:17:51 +0200

    BUMP

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/sketches/[id]":["static\u002Fchunks\u002Fpages\u002Fsketches\u002F[id].js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ricardomatias.github.io
 my personal website
+
+## TODO
+
+* Create proper footer
+* Adjust size of the thumbnail in the gallery
+* Fill the project's descriptions
+* Add about page
+* Add Impressum/blabla to the footer
+* Add project with video
